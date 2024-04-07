ebdb01776d1521be6e10fd52c458e26740c6870a
Ali Can KANBOLAT <kanbolat.alican@gmail.com>
2024-04-07 14:19:16 +0300

    props-destructering-and-operator-ternary-operaor-conditional-rendering-practices

---
+++ conditional-rendering-practice/src/components/Form.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Form({isRegistered}) {
+  return (
+    <form className="form">
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      {isRegistered === false && <input type="password" placeholder="Confirm Password" />}
+      <button type="submit">{isRegistered===true ? "Login" : "Register"}</button>
+    </form>
+  );
+}
+
+export default Form;
